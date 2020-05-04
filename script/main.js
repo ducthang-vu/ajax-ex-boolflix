d215cf624b68ab3ac8c1ec46ebf32768f58ea897
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-04 14:06:41 +0200

    main task completed

--- script/main.js
+++ script/main.js
@@ -1,17 +1,41 @@
 console.log('main.js is working');
 console.log($);
 
+
+function gettingMovies(query, template) {
+    $.ajax({
+        url: "https://api.themoviedb.org/3/search/movie",
+        method: 'GET',
+        data: {
+            api_key: '1e44d8a8c31825e9df1bd0cab47e61e5',
+            query: query,
+            language: 'it-IT'
+        },
+        success: (response) => {
+            console.log(response.results)
+            response.results.forEach(movie => {
+                var context = {
+                    title: movie.title,
+                    title_or: movie.original_title,
+                    language: movie.original_language,
+                    vote: movie.vote_average
+                };
+
+                $('.main-content').append(template(context));
+            });
+        },
+        error: () => console.log('API error')
+    });
+}
+
+
 $(document).ready(function () {
     var source = $('#template-movie-card').html();
     var template = Handlebars.compile(source);
     
-    //test
-    var context = {
-        title: "titolo",
-        title_or: "titolo2",
-        language: "lingua",
-        vote: "voto"
-    }
-
-    $('.main-content').append(template(context))
+    $('#search-btn').click(() => {
+        console.log('btn pressed')
+        console.log($('#search-input').val())
+        gettingMovies($('#search-input').val(), template)
+    })
 });
