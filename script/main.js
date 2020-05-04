5da89082525ea1ce2779b5da45ac0d7fa9673281
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-04 12:49:39 +0200

    template created

--- script/main.js
+++ script/main.js
@@ -1,2 +1,17 @@
-console.log('main.js is working')
-console.log($)
+console.log('main.js is working');
+console.log($);
+
+$(document).ready(function () {
+    var source = $('#template-movie-card').html();
+    var template = Handlebars.compile(source);
+    
+    //test
+    var context = {
+        title: "titolo",
+        title_or: "titolo2",
+        language: "lingua",
+        vote: "voto"
+    }
+
+    $('.main-content').append(template(context))
+});
